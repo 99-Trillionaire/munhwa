0e38f8fb28026341007293a435fe7ea74528b217
COYO <dayeon.yoon@gmail.com>
2024-07-29 14:40:06 +0900

    build: vanilla-extract 추가 및 초기 설정

--- munhwa-client/next.config.mjs
+++ munhwa-client/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import("next").NextConfig} */
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
 const nextConfig = {
 	reactStrictMode: true,
-	output: "standalone",
+	output: "standalone"
 };
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
